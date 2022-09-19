5ec367c0cfcb43d47055b1c37d965552739d71cb
[github <dokyungdavidan@gmail.com>
2022-09-20 00:01:20 +0900

    Test code completed

---
+++ src/evenAndOdd/EvenAndOdd.java
@@ -0,0 +1,7 @@
+package evenAndOdd;
+
+public class EvenAndOdd {
+	public String evenAndOdd(int num) {
+		return (num % 2 == 0) ? "Even" : "Odd";
+	}
+}
---
+++ src/evenAndOdd/EvenAndOddApplication.java
@@ -0,0 +1,11 @@
+package evenAndOdd;
+
+public class EvenAndOddApplication {
+	public static void main(String args[]) {
+		EvenAndOdd eao = new EvenAndOdd();
+		
+		// Test print results
+		System.out.println("Result for 3 = " + eao.evenAndOdd(3));
+		System.out.println("Result for 4 = " + eao.evenAndOdd(4));
+	}
+}
